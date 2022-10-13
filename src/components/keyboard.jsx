3884a2cd4f1d5ccf7a725b99622279cad7e3b45b
abdullah <abdullah@abdullahs-MacBook-Pro.local>
2022-10-13 02:28:11 +0100

    added titles and borders to sidebar keyboards added displayTitle property to sidebar keyboards and their keys added titles to sidebar buttons positioned titles using grid only on desktop

--- src/components/keyboard.jsx
+++ src/components/keyboard.jsx
@@ -1,37 +1,47 @@
 import React from "react";
 import { Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 import Key from "./key";
 
-export function Keyboard({props, selected, xs, md, lg}) {
-  const ref = React.createRef();
+export function Keyboard({ props, selected, xs, md, lg }) {
+    const ref = React.createRef();
 
-  props.keys.forEach((key) => {
-    if (selected && selected === key.id) {
-      key.selected = true;
+    props.keys.forEach((key) => {
+        if (selected && selected === key.id) {
+            key.selected = true;
+        } else {
+            key.selected = false;
+        }
+    });
+    let title;
+    if (props.showTitle) {
+        title = (
+            <Typography className="sidebar_kb_title">{props.name}</Typography>
+        );
     } else {
-      key.selected = false;
+        title = "";
     }
-  });
-  return (
-    <Grid
-      item={true}
-        xs={xs}
-        md={md}
-        lg={lg}
-        className={`keyboard ${props.className}`}
-      >
-        {props.keys.map((ky) => {
-          return (
-            <Key
-              ref={ref}
-              key={ky.id}
-              kObj={ky}
-              handleClick={props.onClick}
-              keyErr={props.keyErr}
-            />
-          );
-        })}
-      </Grid>
-  );
+    return (
+        <Grid
+            item={true}
+            xs={xs}
+            md={md}
+            lg={lg}
+            className={`keyboard ${props.className}`}
+        >
+            {title}
+            {props.keys.map((ky) => {
+                return (
+                    <Key
+                        ref={ref}
+                        key={ky.id}
+                        kObj={ky}
+                        handleClick={props.onClick}
+                        keyErr={props.keyErr}
+                    />
+                );
+            })}
+        </Grid>
+    );
 }
